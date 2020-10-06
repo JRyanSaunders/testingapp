508e54646bd054075f14d769d3dfe5ced1e2947d
System Administrator <root@jacks-mbp.home>
2020-10-06 20:09:25 +0100

    First commit

--- App.js
+++ App.js
@@ -1,8 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  let person = {
+    name: "blah",
+    age: 20,
+  };
+
+  let details = {
+    car: "blah",
+    food: "blah",
+  };
+
+  let information = {
+    ...person,
+    ...details,
+  };
+
+  console.log(information);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -14,8 +31,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
